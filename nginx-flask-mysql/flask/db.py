cae9434c58312ad078f123b2ea52b2dd35b35ed5
DAMA1011 <oldguy911011@gmail.com>
2023-03-07 19:47:55 +0800

    2023/01/17 home

---
+++ nginx-flask-mysql/flask/db.py
@@ -0,0 +1,30 @@
+import pymysql
+
+
+def get_conn():
+    return pymysql.connect(host='db',
+                           user='root',
+                           password='root',
+                           port=3306,
+                           database='test',
+                           charset='utf8')
+
+
+def query_data(sql):
+    conn = get_conn()
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        return cursor.fetchall()
+    finally:
+        conn.close()
+
+
+def inser_or_update__data(sql):
+    conn = get_conn()
+    try:
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+    finally:
+        conn.close()
